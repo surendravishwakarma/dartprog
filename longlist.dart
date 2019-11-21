f4e8ab4d73eb3f8c53e6b592663243d9fa3f551a
surendra <surendra1221994@gmail.com>
2019-11-21 10:48:02 +0530

    long list

---
+++ longlist.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+//import './sec/file2.dart';
+void main() => runApp(MaterialApp(
+ title: "XYZ",
+  debugShowCheckedModeBanner: false,
+  home:Scaffold(
+    appBar: AppBar(
+      title: Text("Listing view"),),
+   body:getView(),
+   //body:glv(),
+  ),
+  //demo(),
+
+));
+
+//Longlist
+List<String> getLVE(){
+   var item=List<String>.generate(1000, (count) => "item $count"); //labda expression and prepared data
+  return item;
+}
+
+Widget getView(){
+
+  var  li=getLVE();
+ // var context;
+  var lv=ListView.builder(itemBuilder:(context, index){
+    return ListTile( title: Text(li[index]),
+
+    );
+
+  }
+
+   );
+    return lv;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+Widget glv(){
+ var lv=ListView (children:<Widget>[
+  ListTile(
+   leading:Icon(Icons.landscape),
+   title: Text("Nature Icons "),
+   subtitle: Text("beautifulview!"),
+   trailing: Icon(Icons.wb_cloudy),
+    onTap: (){
+      debugPrint("Athmin");
+    },
+  ),
+
+   ListTile(
+
+     leading: Icon(Icons.laptop),
+     title: Text("Wimdows"),
+
+
+   ),
+
+
+   ListTile(
+     leading:Icon(Icons.phone_in_talk),
+     title: Text("nice"),
+
+   ),
+
+   Text('show all list view above'),
+   Container(color: Colors.red,height: 20.0,),
+ ]
+
+
+ );
+
+return lv;
+}
